d91572be107f8088d245e83d2c08537d061af4fa
balintcristian <cristi.balint11@gmail.com>
2023-07-19 02:43:45 +0300

    various changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,59 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import customStyles from "./new.module.css";
-
-import jsonData from "../../public/data"; // Assuming you have a separate JSON file with your data
+import jsonData from "../pages/_meta.json"; // Assuming you have a separate JSON file with your data
 
 export default function MyApp() {
+
+  const data = jsonData;
+  
+  // Accessing each child object without creating a local constant
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      console.log(data[key]);
+      const hrefValue = data[key].href;
+      console.log(hrefValue);
+    }
+  }
+  
   return (
     <main className={styles.main}>
       <div className={customStyles.description}>
-          <a id="2">
+{/*           <a id="2">
             Site realizat de{" "}
             <Image
-              src="/inginerie-logo.png"
-              alt="CUNBM logo"
+             src="/inginerie-logo.png"
+  alt="CUNBM logo" 
               className={styles.vercelLogo}
               width={103}
               height={56}
               priority
             />
-          </a>
+          </a>*/}
+          <p>&nbsp;</p>
       </div>
       <div className={styles.center}>
         <div className={styles.grid}>
-          {jsonData.map((item, index) => (
+       {/*}   {jsonData.map((item, index) => (
             <a
-              key={index}
-              href={item.link}
-              className={styles.card}
-              rel="noopener noreferrer"
+              key={item[index]}
+
+href={item.href}
+//onClick={router.push(item.link)}
             >
-              <h2>
-                {item.title} <span>-&gt;</span>
-              </h2>
-              <p>{item.description}</p>
             </a>
-          ))}
+       ))}  */}
+          {Object.keys(data).map((key) => (
+          <a key={key} href={data[key].href} 
+          className={styles.card}
+          rel="noopener noreferrer">
+            <h2>
+                {key} <span>-&gt;</span>
+              </h2>
+              <p>{data[key].title}</p>
+              </a>
+        ))}
         </div>
       </div>
 
