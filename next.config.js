4ee431dc84a45090bc3b1062a3fb0023e52625d8
balintcristian <cristi.balint11@gmail.com>
2023-07-19 03:23:02 +0300

    another one

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:'export',
+    distDir: 'dist',
     reactStrictMode:true,
     swcMinify:true,
     experimental:{
